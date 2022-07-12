7335e5bfeddd7a100567cf7c6fc8ccee8a91307d
ukflava <ukflava@gmail.com>
2022-07-12 15:02:39 -0700

    unit tests and Storybook cases are all passing

---
+++ src/components/DayList.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+  // const dayClass = classNames("day-list__item", {
+  //   // "day-list__item": props,
+  //   "day-list__item--selected": props.selected,
+  //   "day-list__item--full": !props.spots
+    
+  // });
+
+// const formatSpots = (input) => {
+//   if (input === 1 ) { return "1 spot"}
+//   return input === 0 ? "no spots" :  input + " spots"
+// };
+const day = props.days.map(day => {
+  return (
+  <DayListItem
+    key={day.id}
+    name={day.name} 
+    spots={day.spots} 
+    selected={day.name === day} 
+    setDay={day.setDay} />)
+});
+
+
+  return (
+
+<ul>{day}</ul>
+
+    // <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    //   <h2>{props.name}</h2>
+    //   <h3>{formatSpots(props.spots)} remaining</h3>
+    // </li>
+  );
+}
+
+{/* <ul>{<DayListItem />}</ul> */}
