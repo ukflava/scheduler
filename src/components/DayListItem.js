7c91c70a861284593155134b5655f8b7e8b2dea5
ukflava <ukflava@gmail.com>
2022-07-12 13:22:22 -0700

    three CSS classes that we will use with the li element

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,13 +1,19 @@
-export default function DayListItem(props) {
-  // const [day, setDay] = useSTATE()
-  return (
-    <li>
-         onClick={() => {
-      props.setDay(props.name, props.slots)
-         }}
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
-      <h2 className="text--regular">props.name</h2> 
-      <h3 className="text--light">props.spots</h3>
+export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    // "day-list__item": props,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+    
+  });
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2>{props.name}</h2>
+      <h3>{props.spots} spots remaining</h3>
     </li>
   );
 }
