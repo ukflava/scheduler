b63f51f1589552beeea2c3b6750a1f6c05a92bd2
ukflava <ukflava@gmail.com>
2022-07-14 10:06:57 -0700

    add header to index Appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,15 +1,18 @@
 import React, { Fragment } from 'react'
 import "./styles.scss";
-// import Empty from "./Empty.js";
-// import Header from "./Header.js";
-// import Show from "./Show.js";
-// import Confirm from "./Confirm.js";
-// import Status from "./Status.js";
+import Empty from "./Empty.js";
+import Header from "./Header.js";
+import Show from "./Show.js";
+import Confirm from "./Confirm.js";
+import Status from "./Status.js";
 
 export default function Appointment(props) {
   return (
+    <>
+  <Header />
   <article className="appointment"></article>
-  // <Header />
+
+  </>
     );
 
 
