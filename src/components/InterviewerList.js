bccde15c7d0bcd163f42b14bc27ad85517239859
ukflava <ukflava@gmail.com>
2022-07-13 17:55:04 -0700

    finish coding the <InterviewerList> component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,35 +1,39 @@
 import React from "react";
-import classNames from "classnames";
+// import classNames from "classnames";
 
 import InterviewerListItem from "components/InterviewerListItem";
-import "components/InterviewerList.scss";
-
+// import "components/InterviewerList.scss";
+import "components/InterviewerListItem.scss";
 export default function InterviewerList(props) {
-  const InterviewerClass = classNames("interviewers__list", {
-    // "day-list__item": props,
+  // const InterviewerClass = classNames("interviewers__list", {
+  //   // "day-list__item": props,
 
-    // "interviewers__header": props.avatar,
-    // "interviewers__list": props.selected === true
+  //   // "interviewers__header": props.avatar,
+  //   "interviewers__header": props.selected === true
     
     
-  });
+  // });
 
 
-const Interviewers = props.interviewers.map(Interviewer => {
+const Interviewers = props.interviewers.map(interviewer => {
   return (
   <InterviewerListItem
-    key={Interviewer.id}
-    id={Interviewer.id}
-    name={Interviewer.name} 
-    avatar={Interviewer.avatar} 
-    selected={Interviewer.id === props.Interviewer} 
+    key={interviewer.id}
+    id={interviewer.id}
+    name={interviewer.name} 
+    avatar={interviewer.avatar} 
+    selected={interviewer.id === props.interviewer} 
     setInterviewer={props.setInterviewer} />)
 });
 
 
   return (
+    <section className="interviewers">
+  <h4 className="interviewers__header text--light">Interviewer</h4>
+  <ul className="interviewers__list">{Interviewers}</ul>
+</section>
 
-<ul className={InterviewerClass}>{Interviewers}</ul>
+// {/* <ul className={InterviewerClass}>{Interviewers}</ul> */}
 
     // <li className={dayClass} onClick={() => props.setDay(props.name)}>
     //   <h2>{props.name}</h2>
