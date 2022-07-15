5b8c046af17c1b29ee902eb4ba68c74bbc1254b3
ukflava <ukflava@gmail.com>
2022-07-14 18:28:21 -0700

    dailyAppointment + application is now loading data from the API server

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,7 +7,7 @@ import "components/Application.scss";
 //LOCAL IMPORT
 import Appointment from "components/Appointment";
 import DayList from "./DayList";
-
+import { getAppointmentsForDay} from "components/helpers/selectors";
 
 // import InterviewerList from "./InterviewerList";
 // import Appointment from "components/Appointment";
@@ -80,7 +80,7 @@ export default function Application(props) {
     appointments: {}
   });
   const setDay = day => setState({ ...state, day });
-  const dailyAppointments = [];
+  // const dailyAppointments = [];
 
   useEffect(() => {
     Promise.all([
@@ -89,9 +89,11 @@ export default function Application(props) {
       axios.get("http://localhost:8001/api/interviewers")
     ]).then(all => {
      console.log(all)
+     setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, []);
 
+  const dailyAppointments = getAppointmentsForDay(state,state.day)
   
 
   const appointment = dailyAppointments.map((appointment) => {
