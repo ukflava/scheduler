1c1dfd10d9c686f20830012081f81a2f788db872
ukflava <ukflava@gmail.com>
2022-07-16 12:34:30 -0700

    second test for custom hook transition

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,5 +3,15 @@ import React, { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
-  return { mode };
+  function transition(mode, setMode) {
+        setMode("SECOND")
+
+    return {mode}
+
+  }
+
+  return { transition };
+
+ 
 }
+
