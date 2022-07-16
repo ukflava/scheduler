f4093a5e85aa49aa0eb274260e9c434eb24d23c5
ukflava <ukflava@gmail.com>
2022-07-16 14:58:57 -0700

    useMode custom hook not linted

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,17 +4,21 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(value) {
+  function transition(value, replace = false) {
+if (!replace) {
     setHistory(history => [...history, value])
     // return 
     setMode(value)
-       
+  }
+else{
+  setMode(value)
 
     // return {mode}
   }
+}
 
   function back() {
-    console.log("history in back", history)
+    // console.log("history in back", history)
     if (history.length > 1) {
     setMode(history[history.length-2])
     setHistory(history.slice(0,-1))
@@ -25,8 +29,8 @@ export default function useVisualMode(initial) {
 
   }
 // console.log(transition, back)
-console.log(mode)
-console.log("history", history )
+// console.log(mode)
+// console.log("history", history )
 return { mode, transition, back };
 
  
