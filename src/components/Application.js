6cf06d57c21e3b460ddb629e0e8557c80a648001
ukflava <ukflava@gmail.com>
2022-07-18 16:49:51 -0700

    move the logic used to manage the state from the components/Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -10,128 +10,17 @@ import Appointment from "components/Appointment";
 import DayList from "./DayList";
 import { getInterview, getAppointmentsForDay, getInterviewersForDay} from "components/helpers/selectors";
 
-// import InterviewerList from "./InterviewerList";
-// import Appointment from "components/Appointment";
-
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
-
-
-// const appointments = {
-//   "1": {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   "2": {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer:{
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   "3": {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   "4": {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer:{
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   "5": {
-//     id: 5,
-//     time: "4pm",
-//   }
-// };
+const {
+  state,
+  setDay,
+  bookInterview,
+  cancelInterview
+} = useApplicationData();
 
 
 export default function Application(props) {
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    // you may put the line below, but will have to remove/comment hardcoded appointments variable
-    appointments: {},
-    interviewers: {}
-  });
-  const setDay = day => setState({ ...state, day });
-  // const dailyAppointments = [];
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
-    ]).then(all => {
-    //  console.log(all)
-     setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-    //  setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: [] }));
-    // SET to EMPTY ARRAY FOR When the mode === CREATE we want to show the Form component.
-    });
-  }, []);
-
-  function bookInterview(id, interview) {
-    console.log(id, interview);
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    setState({
-      ...state,
-      appointments
-    });
-
-    return axios.put(`/api/appointments/${id}`, { interview })
-  }
-
-  function editInterview(id, interview) {
-    console.log(id, interview);
-    return axios.put(`/api/appointments/${id}`, { interview })
-  }
-
   
-  function cancelInterview(id) {
-    return axios.delete(`/api/appointments/${id}`)}
-
-  // function save(name, interviewer) {
-  //   const interview = {
-  //     student: name,
-  //     interviewer
-  //   };
-  // }
-
   const dailyInterviewers = getInterviewersForDay(state,state.day)
   const dailyAppointments = getAppointmentsForDay(state,state.day)
   
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+export default function useApplicationData(props) {
+const [state, setState] = useState({
+  day: "Monday",
+  days: [],
+  // you may put the line below, but will have to remove/comment hardcoded appointments variable
+  appointments: {},
+  interviewers: {}
+});
+const setDay = day => setState({ ...state, day });
+// const dailyAppointments = [];
+
+useEffect(() => {
+  Promise.all([
+    axios.get("http://localhost:8001/api/days"),
+    axios.get("http://localhost:8001/api/appointments"),
+    axios.get("http://localhost:8001/api/interviewers")
+  ]).then(all => {
+  //  console.log(all)
+   setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+  //  setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: [] }));
+  // SET to EMPTY ARRAY FOR When the mode === CREATE we want to show the Form component.
+  });
+}, []);
+
+function bookInterview(id, interview) {
+  console.log(id, interview);
+  const appointment = {
+    ...state.appointments[id],
+    interview: { ...interview }
+  };
+  const appointments = {
+    ...state.appointments,
+    [id]: appointment
+  };
+
+  setState({
+    ...state,
+    appointments
+  });
+
+  return axios.put(`/api/appointments/${id}`, { interview })
+}
+
+function editInterview(id, interview) {
+  console.log(id, interview);
+  return axios.put(`/api/appointments/${id}`, { interview })
+}
+
+
+function cancelInterview(id) {
+  return axios.delete(`/api/appointments/${id}`)}
+
+// function save(name, interviewer) {
+//   const interview = {
+//     student: name,
+//     interviewer
+//   };
+// }
+}
