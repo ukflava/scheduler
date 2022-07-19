27e63fa8f9abb90563ca333d1b973323cf04ad05
ukflava <ukflava@gmail.com>
2022-07-19 10:09:58 -0700

    add 7 more tests for form

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -3,7 +3,7 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 
 import Form from "components/Appointment/Form";
-
+import { fireEvent } from "@testing-library/react";
 
 
 afterEach(cleanup);
@@ -36,7 +36,33 @@ describe("Form", () => {
   });
 
 
-
+  it("validates that the student name is not blank", () => {
+    /* 1. validation is shown */
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+  
+    /* 2. onSave is not called */
+    expect(onSave).not.toHaveBeenCalled();
+  });
+  
+  it("validates that the interviewer cannot be null", () => {
+    /* 3. validation is shown */
+    expect(getByText(/please select an interviewer/i)).toBeInTheDocument();
+  
+    /* 4. onSave is not called */
+    expect(onSave).not.toHaveBeenCalled();
+  });
+  
+  it("calls onSave function when the name is defined", () => {
+    /* 5. validation is not shown */
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+    expect(queryByText(/please select an interviewer/i)).toBeNull();
+  
+    /* 6. onSave is called once*/
+    expect(onSave).toHaveBeenCalledTimes(1);
+  
+    /* 7. onSave is called with the correct arguments */
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
+  });
 
 
 
