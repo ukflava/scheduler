4f798a6e35f8b0ff3e939d9a312d61bc24bc8dd3
ukflava <ukflava@gmail.com>
2022-07-20 19:17:32 -0700

    coverage report need updates more tests

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -17,47 +17,81 @@ export default function useApplicationData(props) {
 
   const setDay = day => dispatch({ type: SET_DAY, day: day });
 
-useEffect(() => {
-  Promise.all([
-       axios.get("/api/days"),
-    axios.get("/api/appointments"),
-    axios.get("/api/interviewers")
-  ]).then(all => {
-  
-   
+  useEffect(() => {
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(all => {
+      dispatch({
+        type: SET_APPLICATION_DATA,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      });
+    });
+  }, []);
 
-  });
-}, []);
+// function bookInterview(id, interview) {
+//   console.log("interview", state);
+//   const appointment = {
+//     ...state.appointments[id],
+//     interview: { ...interview }
+//   };
+//   const appointments = {
+//     ...state.appointments,
+//     [id]: appointment
+//   };
 
-function bookInterview(id, interview) {
-  console.log("interview", state);
-  const appointment = {
-    ...state.appointments[id],
-    interview: { ...interview }
-  };
-  const appointments = {
-    ...state.appointments,
-    [id]: appointment
-  };
-
-  setState({
-    ...state,
-    appointments
-  });
+//   setState({
+//     ...state,
+//     appointments
+//   });
 
 
   // return axios.put(`api/appointments/${id}`, { interview }).then(console.log("data from spotas update", state))
-  return axios.put(`api/appointments/${id}`, { interview })
-  // .then(mySposts())
-}
+//   return axios.put(`api/appointments/${id}`, { interview })
+//   // .then(mySposts())
+// }
 
 // function editInterview(id, interview) {
 //   console.log(id, interview);
 //   return axios.put(`/api/appointments/${id}`, { interview })
 // }
+
+
+function bookInterview(id, interview) {
+  return axios.put(`/api/appointments/${id}`, { interview }).then(r =>
+    // Promise.all([
+    //   axios.get("/api/appointments"),
+    //   axios.get("/api/days")
+    // ]).then(all => {
+    //   dispatch({
+    //     type: SET_INTERVIEW_DAYS,
+    //     appointments: all[0].data,
+    //     days: all[1].data
+    //   });
+    // })
+    dispatch({
+      type: SET_INTERVIEW,
+      id,
+      interview
+    })
+  );
+}
+
 function cancelInterview(id) {
   return axios.delete(`/api/appointments/${id}`).then(r =>
-
+    // Promise.all([
+    //   axios.get("/api/appointments"),
+    //   axios.get("/api/days")
+    // ]).then(all => {
+    //   dispatch({
+    //     type: SET_INTERVIEW_DAYS,
+    //     appointments: all[0].data,
+    //     days: all[1].data
+    //   });
+    // })
     dispatch({
       type: SET_INTERVIEW,
       id,
@@ -65,7 +99,6 @@ function cancelInterview(id) {
     })
   );
 }
-
 
   
 
